50eb791a94496c3c2910a49163beb95ab72afde6
Luis Forbes <lu@Luiss-iMac.local>
2022-11-29 17:33:08 -0500

    added variables/lists for destinations, restaurants, transportation, and entertainment

---
+++ main.py
@@ -0,0 +1,5 @@
+destinations = ["Bora Bora", "Miami", "Cancun", "Panama", "Puerto Rico", "Egypt"]
+restaurants = ["eat SteakHouse", "get Shake Shack", "eat some Surf n Turf", "eat McDonalds"]
+mode_of_transportation = ["drive a Dodge Challenger", "drive a Chevy Avalanche", "drive a Tahoe", "ride in a taxi"]
+entertainment = ["go bowling", "go to the beach", "go skydiving", "go sight seeing"]
+
